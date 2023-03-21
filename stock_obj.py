4cd45eb707877998cfb1175cf612a6019b7475a6
AJ Goudel <ajgoudel@MacBook-Air.local>
2023-03-21 01:34:16 -0400

    Added for stock_obj.py

--- stock_obj.py
+++ stock_obj.py
@@ -32,21 +32,29 @@ def printStockObj(stockObj):
 
 def readCommonStockData(ticker):
     stockObj = StockObj(ticker)
-    response = requests.get(stockObj.getYahooFinancialDataProfileUrl(ticker), headers=getHeader())
-    print('Common Stock Response Code for ' + ticker + ' is ' + str(response.status_code))
-    soup = BeautifulSoup(response.content, "html.parser")
-    main_section_div = soup.find_all("div", {"class": "asset-profile-container"})
-    div_soup = BeautifulSoup(str(main_section_div), "html.parser")
-    stockObj.fullName = div_soup.find_all('h3')[0].text
-    profile_row = getProfileTexyByTicker(soup, 'Sector(s)', 'span')
-    mini_soup = BeautifulSoup(str(profile_row), "html.parser")
-    span_tags = mini_soup.find_all('span')
-    stockObj.sector = span_tags[1].text
-    stockObj.industry = span_tags[3].text
+    try:
+        response = requests.get(stockObj.getYahooFinancialDataProfileUrl(ticker), headers=getHeader())
+        print('Common Stock Response Code for ' + ticker + ' is ' + str(response.status_code))
+        soup = BeautifulSoup(response.content, "html.parser")
+        main_section_div = soup.find_all("div", {"class": "asset-profile-container"})
+        div_soup = BeautifulSoup(str(main_section_div), "html.parser")
+        stockObj.fullName = div_soup.find_all('h3')[0].text
+        profile_row = getProfileTexyByTicker(soup, 'Sector(s)', 'span')
+        mini_soup = BeautifulSoup(str(profile_row), "html.parser")
+        span_tags = mini_soup.find_all('span')
+        stockObj.sector = span_tags[1].text
+        stockObj.industry = span_tags[3].text
 
-    stock_summary_response = requests.get(stockObj.getYahooFinancialDataQuoteUrl(ticker), headers=getHeader())
-    stock_summary_soup = BeautifulSoup(stock_summary_response.content, "html.parser")
-    stockObj.peRatio = getProfileValueFromRowByText(stock_summary_soup, 'PE Ratio (TTM)', 'span')
-    stockObj.dividend = getProfileValueFromRowByText(stock_summary_soup, 'Forward Dividend & Yield', 'span')
-    printStockObj(stockObj)
-    return stockObj
+        stock_summary_response = requests.get(stockObj.getYahooFinancialDataQuoteUrl(ticker), headers=getHeader())
+        stock_summary_soup = BeautifulSoup(stock_summary_response.content, "html.parser")
+        stockObj.peRatio = getProfileValueFromRowByText(stock_summary_soup, 'PE Ratio (TTM)', 'span')
+        stockObj.dividend = getProfileValueFromRowByText(stock_summary_soup, 'Forward Dividend & Yield', 'span')
+        printStockObj(stockObj)
+        return stockObj
+    except:
+        stockObj.industry = 'ERROR'
+        stockObj.sector = 'ERROR'
+        stockObj.peRatio = 'ERROR'
+        stockObj.dividend = 'ERROR'
+        stockObj.fullName = 'ERROR'
+        return stockObj
